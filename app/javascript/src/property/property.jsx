f5003435f869a8a411d6dd7034b12522bc7cb41a
da3491 <d.annese3491@gmail.com>
2022-11-10 14:05:34 -0500

    Debugging

--- app/javascript/src/property/property.jsx
+++ app/javascript/src/property/property.jsx
@@ -21,6 +21,8 @@ class Property extends React.Component {
                     loading: false,
                 })
             })
+
+        console.log(process.env.STRIPE_PUBLISHABLE_KEY)
     }
 
     render() {
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -11,6 +11,21 @@ const customConfig = {
   }
 }
 
+// Add this to choose which .env vairables to expose to the fronend
+const webpack = require('webpack')
+
+environment.plugins.prepend(
+  "Environment",
+  new webpack.EnvironmentPlugin(
+    JSON.parse(
+      JSON.stringify({
+        STRIPE_PUBLISHABLE_KEY: process.env.STRIPE_PUBLISHABLE_KEY,
+        URL: process.env.URL
+      })
+    )
+  )
+);
+
 environment.config.merge(customConfig);
 
 environment.splitChunks()
